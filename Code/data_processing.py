d83df8023af84061099cf00f2892ce53782b3096
Lincoln <51924537+lincoln-cmd@users.noreply.github.com>
2024-09-24 20:17:32 +0900

    Add files via upload

---
+++ Code/data_processing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.font_manager as fm
+
+
+df = pd.read_csv('FDS-Trnasaction(Lat,Long).csv', encoding='utf-8')
+
+print(df)
